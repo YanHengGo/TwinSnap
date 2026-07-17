b8ef25f2bf0425c654c0d23399051910ed23c04d
yanheng <dang712@hotmail.com>
2026-07-18 06:50:52 +0900

    Initial Commit

---
+++ TwinSnap/TwinSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwinSnapApp.swift
+//  TwinSnap
+//
+//  Created by 厳恒 on 2026/07/18.
+//
+
+import SwiftUI
+
+@main
+struct TwinSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
